1c9beaff7d2dd83a1e718e9638b0062a7164f7a5
Rafael Sánchez Córdoba <53056368+RafaelSanchezCordoba@users.noreply.github.com>
2022-04-08 12:45:35 +0200

    Model implementation
    I've created the first version of the model.

---
+++ Implementation/Client/src/model/CurrentTime.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * A class that can return the current date and time formatted in various standard formats.
+ */
+public class CurrentTime {
+    private static final DateTimeFormatter EUROPEAN_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private static final DateTimeFormatter ISO_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy_MM_dd");
+
+    public CurrentTime() {
+    }
+
+    public String getFormattedTime() {
+        LocalDateTime time = LocalDateTime.now();
+        return time.format(EUROPEAN_TIME_FORMATTER);
+    }
+
+    public String getFormattedIsoDate() {
+        LocalDateTime time = LocalDateTime.now();
+        return time.format(ISO_DATE_FORMATTER);
+    }
+}
