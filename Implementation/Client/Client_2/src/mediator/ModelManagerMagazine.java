71c55213a2e358c765c2cadfe0cbd004d853f6e5
kimintor <92308490+kimintor@users.noreply.github.com>
2022-04-13 20:52:54 +0200

    kk

--- Implementation/Client/Client_2/src/mediator/ModelManagerMagazine.java
+++ Implementation/Client/Client_2/src/mediator/ModelManagerMagazine.java
@@ -7,42 +7,85 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.rmi.RemoteException;
 
+<<<<<<< HEAD
 
+=======
+/**
+ * The model manager for magazine.
+ * @author Rafael Sánchez Córdoba.
+ * @version 1.0 09/04/22
+ */
+>>>>>>> 140f6a0c353514d0b2e9badd881b8031c2386050
 public class ModelManagerMagazine implements ModelMagazine{
     private final MagazineClient client;
     private final PropertyChangeSupport support;
 
+<<<<<<< HEAD
 
+=======
+    /**
+     *
+     * @param client
+     */
+>>>>>>> 140f6a0c353514d0b2e9badd881b8031c2386050
     public ModelManagerMagazine(MagazineClient client) {
         this.client = client;
         this.support = new PropertyChangeSupport(this);
     }
 
+    /**
+     *
+     * @param magazine
+     * @throws RemoteException
+     */
     @Override
     public void addMagazine(Magazine magazine) throws RemoteException {
         client.addMagazine(magazine);
     }
 
+    /**
+     *
+     * @param id
+     * @throws RemoteException
+     */
     @Override
     public void removeMagazine(int id) throws RemoteException {
         client.removeMagazine(id);
     }
 
+    /**
+     *
+     * @param listener
+     */
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
 
+    /**
+     *
+     * @param name
+     * @param listener
+     */
     @Override
     public void addPropertyChangeListener(String name, PropertyChangeListener listener) {
         support.addPropertyChangeListener(name, listener);
     }
 
+    /**
+     *
+     * @param listener
+     */
     @Override
     public void removePropertyChangeListener(PropertyChangeListener listener) {
         support.removePropertyChangeListener(listener);
     }
 
+    /**
+     *
+     * @param name
+     * @param listener
+     */
     @Override
     public void removePropertyChangeListener(String name, PropertyChangeListener listener) {
         support.removePropertyChangeListener(name, listener);
--- Implementation/Client/Shared/src/model/MultimediaItem.java
+++ Implementation/Client/Shared/src/model/MultimediaItem.java
@@ -4,10 +4,16 @@ import model.GenreList;
 
 import java.io.Serializable;
 
+/**
+ * MultimediaItem class that implements <code>Serializable</code>.
+ * @author Rafael Sánchez Córdoba.
+ * @version 1.0 08/04/22.
+ */
 public class MultimediaItem implements Serializable {
     private final int id;
     private final String title, publisher;
 
+<<<<<<< HEAD
     public int getId()
     {
         return id;
@@ -23,6 +29,17 @@ public class MultimediaItem implements Serializable {
         return publisher;
     }
 
+=======
+    /**
+     * Multimedia item constructor
+     * @param id
+     * The unique identification number of the multimedia item,
+     * @param title
+     * The title of the multimedia item.
+     * @param publisher
+     * The publisher of the multimedia item.
+     */
+>>>>>>> 140f6a0c353514d0b2e9badd881b8031c2386050
     public MultimediaItem(int id, String title, String publisher) {
         this.id = id;
         this.title = title;
