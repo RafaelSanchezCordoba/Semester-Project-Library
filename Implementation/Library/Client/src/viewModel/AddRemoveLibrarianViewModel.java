74e4fe4ae848d426684ffd50de80dabc1a8d3e12
318212 <318212@via.dk>
2022-05-04 10:28:45 +0200

    Update AddRemoveLibrarianViewModel.java

--- Implementation/Library/Client/src/viewModel/AddRemoveLibrarianViewModel.java
+++ Implementation/Library/Client/src/viewModel/AddRemoveLibrarianViewModel.java
@@ -18,83 +18,85 @@ public class AddRemoveLibrarianViewModel implements PropertyChangeListener
 {
 
 
-  private final ModelLibrarian model;
+    private final ModelLibrarian model;
 
-  private final StringProperty firstNameTextField;
-  private final StringProperty lastNameTextField;
-  private final StringProperty passwordTextField;
-  private final StringProperty ssnTextField;
-  private final StringProperty errorLabel;
-  private final ObservableList options ;
-  public AddRemoveLibrarianViewModel(ModelLibrarian model){
+    private final StringProperty firstNameTextField;
+    private final StringProperty lastNameTextField;
+    private final StringProperty passwordTextField;
+    private final StringProperty ssnTextField;
+    private final StringProperty errorLabel;
+    private final ObservableList options ;
+    public AddRemoveLibrarianViewModel(ModelLibrarian model){
 
-    this.model = model;
-    this.firstNameTextField=new SimpleStringProperty("");
-    this.lastNameTextField = new SimpleStringProperty("");
-    this.passwordTextField = new SimpleStringProperty("");
-    this.ssnTextField = new SimpleStringProperty("");
-    this.errorLabel = new SimpleStringProperty("");
-    this.options = FXCollections.observableArrayList();
-    try
-    {
-      options.addAll(model.getLibrarianList());
-    }catch (RemoteException e){
-      e.printStackTrace();
-      errorLabel.set("unable to access librarian List");
-    }
-  }
-
-  public ObservableList getOptions(){
-    return options;
-  }
-
-  public void removeLibrarian(Librarian librarian){
-    try
-    {
-      model.removeLibrarian(librarian);
-    }catch (RemoteException e){
-      e.printStackTrace();
-    }
-  }
-
-  public void addLibrarian(){
-    try
-    {
-
-      model.addLibrarian(new Librarian(firstNameTextField.getValue(),
-          lastNameTextField.getValue(), passwordTextField.getValue(),
-          ssnTextField.getValue()));
-
-    }catch (RemoteException e){
-      e.printStackTrace();
-      errorLabel.set("unable to add Librarian");
+        this.model = model;
+        this.firstNameTextField=new SimpleStringProperty("");
+        this.lastNameTextField = new SimpleStringProperty("");
+        this.passwordTextField = new SimpleStringProperty("");
+        this.ssnTextField = new SimpleStringProperty("");
+        this.errorLabel = new SimpleStringProperty("");
+        this.options = FXCollections.observableArrayList();
+        try
+        {
+            options.addAll(model.getLibrarianList());
+        }catch (RemoteException e){
+            e.printStackTrace();
+            errorLabel.set("unable to access librarian List");
+        }
     }
 
-  }
+    public ObservableList getOptions(){
+        return options;
+    }
 
-  public Librarian getInstance(){
-    return new Librarian(firstNameTextField.getValue(),
-        lastNameTextField.getValue(), passwordTextField.getValue(),
-        ssnTextField.getValue());
-  }
-  public void bindFirstNameTextField(StringProperty property) {
-    property.bindBidirectional(firstNameTextField);
-  }
-  public void bindLastNameTextField(StringProperty property ){
-    property.bindBidirectional(lastNameTextField);
-  }
-  public void bindPasswordTextField(StringProperty property){
-    property.bindBidirectional(passwordTextField);
-  }
-  public void bindSsnTextField(StringProperty property){
-   property.bindBidirectional(ssnTextField);
-  }
-  public void bindErrorLabelTextField(StringProperty property){
-    property.bindBidirectional(errorLabel);
-  }
+    public void removeLibrarian(Librarian librarian){
+        try
+        {
+            model.removeLibrarian(librarian);
+        }catch (RemoteException e){
+            e.printStackTrace();
+        }
+    }
 
-  @Override public void propertyChange(PropertyChangeEvent evt)
-  {
+    public void addLibrarian(){
+        try
+        {
 
-  }
+            model.addLibrarian(new Librarian(firstNameTextField.getValue(),
+                    lastNameTextField.getValue(), passwordTextField.getValue(),
+                    ssnTextField.getValue()));
+
+        }catch (RemoteException e){
+            e.printStackTrace();
+            errorLabel.set("unable to add Librarian");
+        }
+
+    }
+
+    public Librarian getInstance(){
+        return new Librarian(firstNameTextField.getValue(),
+                lastNameTextField.getValue(), passwordTextField.getValue(),
+                ssnTextField.getValue());
+    }
+    public void bindFirstNameTextField(StringProperty property) {
+        property.bindBidirectional(firstNameTextField);
+    }
+    public void bindLastNameTextField(StringProperty property ){
+        property.bindBidirectional(lastNameTextField);
+    }
+    public void bindPasswordTextField(StringProperty property){
+        property.bindBidirectional(passwordTextField);
+    }
+    public void bindSsnTextField(StringProperty property){
+        property.bindBidirectional(ssnTextField);
+    }
+    public void bindErrorLabelTextField(StringProperty property){
+        property.bindBidirectional(errorLabel);
+    }
+
+    @Override public void propertyChange(PropertyChangeEvent evt)
+    {
+
+    }
 }
+
+
