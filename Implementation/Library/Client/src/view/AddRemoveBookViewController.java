27ef220987eb3913acc129d5c0322af9c29f7f06
Rafael Sánchez Córdoba <53056368+RafaelSanchezCordoba@users.noreply.github.com>
2022-05-12 13:40:28 +0200

    Update AddRemoveBookViewController.java

--- Implementation/Library/Client/src/view/AddRemoveBookViewController.java
+++ Implementation/Library/Client/src/view/AddRemoveBookViewController.java
@@ -1,7 +1,5 @@
 package view;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.Region;
@@ -12,7 +10,6 @@ import viewModel.AddRemoveBookViewModel;
 
 import java.rmi.RemoteException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class AddRemoveBookViewController
 {
