7b3a9bcbd9ab671e25ff0e01663c511eb50e0a80
318212 <318212@via.dk>
2022-05-12 14:56:38 +0200

    vm

--- Implementation/Library/Client/src/view/AddRemoveLibraryUserViewController.java
+++ Implementation/Library/Client/src/view/AddRemoveLibraryUserViewController.java
@@ -1,5 +1,21 @@
 package view;
 
+import viewModel.AddRemoveLibraryUserViewModel;
+
+import javax.swing.plaf.synth.Region;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+
 public class AddRemoveLibraryUserViewController
 {
+    private ViewHandler viewHandler;
+    private Region root;
+    private AddRemoveLibraryUserViewModel viewModel;
+
+
+    public void init(ViewHandler viewHandler, AddRemoveLibraryUserViewModel viewModel, Region root) throws SQLException, RemoteException{
+        this.viewHandler = viewHandler;
+        this.viewModel = viewModel;
+        this.root = root;
+    }
 }
