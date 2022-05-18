b88528855d6d6009ba6a79f0d636c3b427926ac4
kimintor <92308490+kimintor@users.noreply.github.com>
2022-05-18 08:57:50 +0200

    ..

---
+++ Implementation/Library/Client/src/mediator/ModelLoanBook.java
@@ -0,0 +1,15 @@
+package mediator;
+
+import model.Book;
+import model.LoanBook;
+
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface ModelLoanBook extends PropertyChangeSubject
+{
+  void addLoanBook(LoanBook loanBook) throws SQLException, RemoteException;
+  ArrayList<Book> getAvailableBooks() throws SQLException, RemoteException;
+}
+
---
+++ Implementation/Library/Client/src/mediator/ModelManagerLoanBook.java
@@ -0,0 +1,59 @@
+package mediator;
+
+import client.LoanBookClient;
+import model.Book;
+import model.LoanBook;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ModelManagerLoanBook implements ModelLoanBook
+{
+  private final LoanBookClient client;
+  private PropertyChangeSupport support;
+
+  public ModelManagerLoanBook(LoanBookClient loanBookClient){
+    this.client = loanBookClient;
+    this.support = new PropertyChangeSupport(this);
+  }
+
+  @Override public void addLoanBook(LoanBook loanBook)
+      throws SQLException, RemoteException
+  {
+    client.addBookLoan(loanBook);
+    support.firePropertyChange("newLoanBook",null,loanBook);
+  }
+
+  @Override public ArrayList<Book> getAvailableBooks()
+      throws SQLException, RemoteException
+  {
+    return client.getAvailableBookList();
+  }
+
+  @Override public void addPropertyChangeListener(
+      PropertyChangeListener listener)
+  {
+
+  }
+
+  @Override public void addPropertyChangeListener(String name,
+      PropertyChangeListener listener)
+  {
+
+  }
+
+  @Override public void removePropertyChangeListener(
+      PropertyChangeListener listener)
+  {
+
+  }
+
+  @Override public void removePropertyChangeListener(String name,
+      PropertyChangeListener listener)
+  {
+
+  }
+}
