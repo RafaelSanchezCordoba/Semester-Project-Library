c45872eb64970eb211ace0a653613c6ec0323713
Rafael Sánchez Córdoba <53056368+RafaelSanchezCordoba@users.noreply.github.com>
2022-05-23 13:41:12 +0200

    Update LoanBookClientImplementation.java

--- Implementation/Library/Client/src/client/LoanBookClientImplementation.java
+++ Implementation/Library/Client/src/client/LoanBookClientImplementation.java
@@ -19,6 +19,15 @@ public class LoanBookClientImplementation extends UnicastRemoteObject implements
 {
   private RemoteLoanBook remoteLoanBook;
 
+  /**
+   * The constructor that create and set the Registry.
+   * @param host
+   * The name of the host
+   * @param port
+   * The port number
+   * @throws IOException
+   * @throws NotBoundException
+   */
   public LoanBookClientImplementation(String host, int port) throws IOException,
       NotBoundException
   {
@@ -26,35 +35,73 @@ public class LoanBookClientImplementation extends UnicastRemoteObject implements
     remoteLoanBook = (RemoteLoanBook) registry.lookup("loanBook");
   }
 
+  /**
+   * Add loan book method
+   * @param loanBook
+   * The loan book passed as an argument
+   * @throws SQLException
+   * @throws RemoteException
+   */
   @Override public void addBookLoan(LoanBook loanBook)
       throws SQLException, RemoteException
   {
     remoteLoanBook.addBookLoan(loanBook);
   }
 
+  /**
+   * Get the available books
+   * @return
+   * All the available books
+   * @throws SQLException
+   * @throws RemoteException
+   */
   @Override public ArrayList<Book> getAvailableBookList()
       throws SQLException, RemoteException
   {
     return remoteLoanBook.getAvailableBookList();
   }
 
+  /**
+   * Get a user with a specific social security number
+   * @param ssn
+   * The social security number passed as an argument
+   * @return
+   * @throws RemoteException
+   */
   @Override public LibraryUser getUser(String ssn)
       throws RemoteException
   {
      return  remoteLoanBook.getUser(ssn);
   }
 
+  /**
+   * Get the loan of the user with the social security number passed as an argument
+   * @param ssn
+   * The social security number
+   * @return
+   * @throws RemoteException
+   */
   @Override public ArrayList<LoanBook> getUserBookLoans(String ssn)
       throws RemoteException
   {
     return remoteLoanBook.getUserBookLoans(ssn);
   }
 
+  /**
+   * Return book method
+   * @param loan_id
+   * The unique identification number of the loan
+   * @throws RemoteException
+   */
   @Override public void returnBook(int loan_id) throws RemoteException
   {
     remoteLoanBook.returnBook(loan_id);
   }
 
+  /**
+   * Close the remote object
+   * @throws IOException
+   */
   @Override public void close() throws IOException
   {
     UnicastRemoteObject.unexportObject(this,true);
