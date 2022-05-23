f0868b9a68df652083bf83aafb51e301c1b17d81
Rafael Sánchez Córdoba <53056368+RafaelSanchezCordoba@users.noreply.github.com>
2022-05-23 13:44:09 +0200

    LoanMagazineClient JavaDoc

--- Implementation/Library/Client/src/client/LoanMagazineClient.java
+++ Implementation/Library/Client/src/client/LoanMagazineClient.java
@@ -10,9 +10,47 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface LoanMagazineClient extends Closeable {
+    /**
+     * Add loan magazine method
+     * @param loanMagazine
+     * The loan magazine passed as an argument
+     * @throws SQLException
+     * @throws RemoteException
+     */
     void addMagazineLoan(LoanMagazine loanMagazine) throws  RemoteException, SQLException;
+
+    /**
+     * Get the available magazines
+     * @return
+     * All the available magazines
+     * @throws SQLException
+     * @throws RemoteException
+     */
     ArrayList<Magazine> getAvailableMagazineList() throws  RemoteException, SQLException;
+
+    /**
+     * Get a user with a specific social security number
+     * @param ssn
+     * The social security number passed as an argument
+     * @return
+     * @throws RemoteException
+     */
     LibraryUser getUser(String ssn) throws RemoteException;
+
+    /**
+     * Get the loan of the user with the social security number passed as an argument
+     * @param ssn
+     * The social security number
+     * @return
+     * @throws RemoteException
+     */
     ArrayList<LoanMagazine> getUserLoans(String ssn) throws RemoteException;
+
+    /**
+     * Return magazine method
+     * @param loan_id
+     * The unique identification number of the loan
+     * @throws RemoteException
+     */
     void returnMagazine(int loan_id) throws RemoteException;
 }
--- Implementation/Library/Client/src/client/LoanMagazineClientImplementation.java
+++ Implementation/Library/Client/src/client/LoanMagazineClientImplementation.java
@@ -17,38 +17,85 @@ import java.util.ArrayList;
 public class LoanMagazineClientImplementation extends UnicastRemoteObject implements LoanMagazineClient{
     private RemoteLoanMagazine remoteLoanMagazine;
 
+    /**
+     * The constructor that create and set the Registry.
+     * @param host
+     * The name of the host
+     * @param port
+     * The port number
+     * @throws IOException
+     * @throws NotBoundException
+     */
     public LoanMagazineClientImplementation(String  host, int port) throws IOException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(host,port);
         remoteLoanMagazine = (RemoteLoanMagazine) registry.lookup("loanMagazine");
 
     }
 
+    /**
+     * Add loan magazine method
+     * @param loanMagazine
+     * The loan magazine passed as an argument
+     * @throws SQLException
+     * @throws RemoteException
+     */
     @Override
     public void addMagazineLoan(LoanMagazine loanMagazine) throws SQLException, RemoteException {
         remoteLoanMagazine.addMagazineLoan(loanMagazine);
     }
 
+    /**
+     * Get the available magazines
+     * @return
+     * All the available magazines
+     * @throws SQLException
+     * @throws RemoteException
+     */
     @Override
     public ArrayList<Magazine> getAvailableMagazineList() throws  RemoteException,SQLException {
         return remoteLoanMagazine.getAvailableMagazineList();
     }
 
+    /**
+     * Get a user with a specific social security number
+     * @param ssn
+     * The social security number passed as an argument
+     * @return
+     * @throws RemoteException
+     */
     @Override public LibraryUser getUser(String ssn)
         throws RemoteException
     {
         return remoteLoanMagazine.getUser(ssn);
     }
 
+    /**
+     * Get the loan of the user with the social security number passed as an argument
+     * @param ssn
+     * The social security number
+     * @return
+     * @throws RemoteException
+     */
     @Override
     public ArrayList<LoanMagazine> getUserLoans(String ssn) throws RemoteException {
         return remoteLoanMagazine.getUserLoans(ssn);
     }
 
+    /**
+     * Return magazine method
+     * @param loan_id
+     * The unique identification number of the loan
+     * @throws RemoteException
+     */
     @Override public void returnMagazine(int loan_id) throws RemoteException
     {
             remoteLoanMagazine.returnMagazine(loan_id);
     }
 
+    /**
+     * Close the remote object
+     * @throws IOException
+     */
     @Override
     public void close() throws IOException {
         UnicastRemoteObject.unexportObject(this,true);
